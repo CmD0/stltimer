bcb3f78b4a704365e27618af911e119ef9eecb33
unknown <elouan112@gmail.com>
2022-09-01 21:18:30 -0400

    initial commit

---
+++ src/main/java/com/example/stltimer/StltimerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stltimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StltimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StltimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stltimer/StltimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stltimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StltimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
